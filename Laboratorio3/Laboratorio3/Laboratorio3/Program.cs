bfe4d81c648fc203aeb760c0811ec624623ac590
francisco <francisco_0699@hotmail.com>
2025-09-20 13:03:21 -0500

    Commit

---
+++ Laboratorio3/Laboratorio3/Laboratorio3/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Laboratorio3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CalculosMatematicos calculo = new CalculosMatematicos();
+
+            Console.Write("Introduce el primer número: ");
+            calculo.primerNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Introduce el segundo número: ");
+            calculo.segundoNumero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("El resultado de ({0}+{1})*({0}-{1}) es: {2}", calculo.primerNumero, calculo.segundoNumero, calculo.calcular());
+        }
+    }
+
+    public class CalculosMatematicos
+    {
+        //Declarando variables de instancia en clase.
+        public int primerNumero { get; set; }
+        public int segundoNumero { get; set; }
+
+        public double calcular()
+        {
+            //Utilizando variables de instancia dentro de metodos de la clase.
+            return (primerNumero + segundoNumero) * (primerNumero - segundoNumero);
+        }
+    }
+
+}
