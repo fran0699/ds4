b3289cdf96ac796f04187922205b1e04cc059747
francisco <francisco_0699@hotmail.com>
2025-09-20 15:26:15 -0500

    Commit completo.

---
+++ Laboratorio3/Laboratorio4/Laboratorio43/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Laboratorio43
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la nota del estudiante");
+            float score = float.Parse(Console.ReadLine());
+
+            if (score >= 70)
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score}, usted ha aprobado");
+            }
+            else
+            {
+                Console.WriteLine();
+                Console.WriteLine($"Su nota es {score}, usted ha reprobado, debe repetir");
+            }
+        }
+    }
+}
