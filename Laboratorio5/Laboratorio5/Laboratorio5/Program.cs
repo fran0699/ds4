a80ec7de2bfa18687445561b22a7f1c2bb3deed8
Francisco <francisco_0699@hotmail.com>
2025-09-27 15:24:08 -0500

    Laboratorio completo

---
+++ Laboratorio5/Laboratorio5/Laboratorio5/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    public class PruebaVector1
+    {
+        int[] sueldos = new int[6];
+        public void Cargar()
+        {
+            for (int f = 1; f <= 5; f++)
+            {
+                Console.Write("Ingrese sueldo del operario " + f + ": ");
+                String linea;
+                linea = Console.ReadLine();
+                sueldos[f] = int.Parse(linea);
+            }
+        }
+        public void Imprimir()
+        {
+            Console.Write("Los 5 sueldos de los operarios \n");
+            for (int f = 1; f <= 5; f++)
+            {
+                Console.Write("[" + sueldos[f] + "] ");
+            }
+            Console.ReadKey();
+        }
+    }
+    static void Main(string[] args)
+    {
+        PruebaVector1 pv = new PruebaVector1();
+        pv.Cargar();
+        pv.Imprimir();
+    }
+}
---
+++ Laboratorio5/Laboratorio5/Laboratorio53/Program.cs
@@ -0,0 +1,11 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string[] frutas = { "manzana", "platano", "naranja" };
+        foreach (string fruta in frutas)
+        {
+            Console.WriteLine(fruta);
+        }
+    }
+}
