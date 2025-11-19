f46f78918ec8ca4c97f1bd38e5a6130e7a3e0630
Francisco Lata <francisco_0699@hotmail.com>
2025-11-19 18:27:30 -0500

    Laboratorio 19 completo.

---
+++ Laboratorio19/Laboratorio19/Laboratorio193/Controllers/HomeController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Net;
+using System.Net.Http;
+
+
+
+namespace Laboratorio193.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            string respuestaApi = "";
+            try
+            {
+                string urlBaseApi = "https://localhost:44387/";
+                using (HttpClient clienteHttp = new HttpClient())
+                {
+                    clienteHttp.BaseAddress = new Uri(urlBaseApi);
+                    HttpResponseMessage respuesta = clienteHttp.GetAsync("api/values/get/2").Result;
+
+                    if (respuesta.IsSuccessStatusCode)
+                    {
+                        respuestaApi = respuesta.Content.ReadAsStringAsync().Result;
+                    }
+                    else
+                    {
+                        int codigoEstado = (int)respuesta.StatusCode;
+                        respuestaApi = "Error al llamar al API. Código: "
+                                       + codigoEstado + " - " + respuesta.ReasonPhrase;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                respuestaApi = "Ocurrió una excepción al llamar al API: " + ex.Message;
+            }
+            ViewBag.RespuestaApi = respuestaApi;
+            return View();
+        }
+
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
